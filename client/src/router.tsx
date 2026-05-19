9a6885fa401a1dd6704460a9b2209e277cc120fb
John Patrick Villalon <jocvillalon@yahoo.com.ph>
2026-05-19 21:40:40 +0800

    Fix: Returns removed unused lines

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,7 +1,12 @@
 import { createRouter as createTanStackRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
+// import type { ReactNode } from 'react'
+// import { QueryClient } from '@tanstack/react-query'
 import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query'
+
+// import TanstackQueryProvider from './integrations/tanstack-query/root-provider'
+
 import { getContext } from './integrations/tanstack-query/root-provider'
 
 export function getRouter() {
